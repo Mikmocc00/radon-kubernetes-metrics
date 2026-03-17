d2bb94dfc7bd03fb8fc88559181f77509d0469f5
Alessandro De Cicco <alessandrodecicco02@gmail.com>
2026-03-17 14:52:21 +0100

    khdhkjwd

--- setup.py
+++ setup.py
@@ -31,4 +31,8 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Operating System :: OS Independent",
     ],
+    
+    install_requires=[
+        "pyyaml==6.0.2"
+    ]
 )
